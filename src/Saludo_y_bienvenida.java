ee497d6dc52ce851bbf0a13d01ae788da00fc905
Mauro Aguirre <mauroaguirre095@gmail.com>
2023-08-08 19:54:00 -0300

    Cambios en el saludo

---
+++ src/Saludo_y_bienvenida.java
@@ -0,0 +1,6 @@
+public class Saludo_y_bienvenida {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola mundo!");
+        System.out.println("Bienvenidos a github!");
+    }
+}
